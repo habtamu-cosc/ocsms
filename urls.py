aec7778dcc9aa11ccf1479f055a291fc0b509496
habtamu-cosc <habtaabew12g@gmail.com>
2025-11-25 22:30:50 -0800

    Imports Django’s built-in admin site
    Adde a comment to clarify the import of Django's admin site.

--- urls.py
+++ urls.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+//Imports Django’s built-in admin site
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
